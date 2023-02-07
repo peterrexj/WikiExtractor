be7c8cc7df88488c343b54bc55c6b18c520f4fd4
Joseph, Peter <Peter.Joseph@cba.com.au>
2023-02-07 15:03:32 +1100

    Update tests

--- WikiExtractor.Tests/DataStoreTests.cs
+++ WikiExtractor.Tests/DataStoreTests.cs
@@ -1,11 +1,11 @@
 ﻿using Pj.Library;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using WikiExtractor.DbModels;
+using WikiExtractor.Exts;
+using WikiExtractor.Models;
 using WikiExtractor.Process;
 using WikiExtractor.Repository;
+using WikiExtractor.ViewModels;
 
 namespace WikiExtractor.Tests
 {
@@ -81,25 +81,182 @@ namespace WikiExtractor.Tests
         }
 
         [TestCaseSource(nameof(DatabaseFiles))]
-        public void Should_Have_Data_EachItem(string dbFilePath)
+        public void Should_EachItem_Have_Data(string dbFilePath)
         {
             ProcessConstants.DatabasePath = dbFilePath;
-            WikiAppController wikiAppController = new WikiAppController(new WikiDatabase());
-            var menuItems = wikiAppController.AppMenuItems();
-            foreach (var menuItem in menuItems)
-            {
-                var data = wikiAppController.GetListOfWikiItems(new List<string> { menuItem.Tags });
-                foreach (var item in data)
-                {
+            WikiDatabase wikiDatabase = new WikiDatabase();
+            WikiAppController wikiAppController = new WikiAppController(wikiDatabase);
+            var allMasterData = wikiDatabase.MasterRepository.GetAll();
+            var allPicsData = wikiDatabase.WikiPictureRepository.GetAll();
+            var metadataData = wikiDatabase.MetadataRepository.GetAll();
+            var paraPrimaryData = wikiDatabase.ParagraphPrimaryContentRepository.GetAll();
+            var para2Data = wikiDatabase.ParagraphHeader2Repository.GetAll();
+            var para3Data = wikiDatabase.ParagraphHeader3Repository.GetAll();
+            var paraData = wikiDatabase.ParagraphContentRepository.GetAll();
 
-                    var eachItemDetail = wikiAppController.GetViewModelById(item.Id);
-                    //if (ignoreRouteList.Contains(eachItemDetail.WikiPath)) continue;
-                    Assert.IsNotNull(eachItemDetail);
-                    Assert.IsTrue(eachItemDetail.Name.HasValue());
-                    Assert.IsTrue(eachItemDetail.WikiPath.HasValue());
-                    Assert.IsFalse(eachItemDetail.Pictures.IsEmpty() &&
-                        eachItemDetail.Metadatas.IsEmpty() &&
-                        eachItemDetail.Pictures.IsEmpty());
+            Parallel.ForEach(allMasterData, new ParallelOptions { MaxDegreeOfParallelism = 10 }, item =>
+            {
+                var data = GetViewModelv2Test(item.Id,
+                    allMasterData, allPicsData, metadataData, paraPrimaryData, para2Data, para3Data, paraData);
+                Assert.IsNotNull(data);
+                Assert.IsTrue(data.Name.HasValue());
+                Assert.IsTrue(data.WikiPath.HasValue());
+                Assert.IsFalse(data.Pictures.IsEmpty() &&
+                    data.Metadatas.IsEmpty() &&
+                    data.Pictures.IsEmpty());
+                Assert.IsTrue(data.Paragraphs.Any());
+            });
+        }
+
+        private PersonaViewModel GetViewModelv2Test(int masterId,
+            IEnumerable<Master> maData,
+            IEnumerable<WikiPicture> allPicsData,
+            IEnumerable<Metadata> metadataData,
+            IEnumerable<ParagraphPrimaryContent> paraPrimaryData,
+            IEnumerable<ParagraphHeader2> para2Data,
+            IEnumerable<ParagraphHeader3> para3Data,
+            IEnumerable<ParagraphContent> paraData)
+        {
+            var persona = (from master in maData
+
+                           join picJoin in allPicsData on master.Id equals picJoin.MasterId into picGroup
+                           from pic in picGroup.DefaultIfEmpty(new WikiPicture { MasterId = master.Id, Path = "NoImageAvailable.png", Caption = string.Empty })
+
+                           join metadataJoin in metadataData on master.Id equals metadataJoin.MasterId into metadataGrp
+                           from metadata in metadataGrp.DefaultIfEmpty(new Metadata { Id = 0, MasterId = master.Id })
+
+                           join mainCont in paraPrimaryData on master.Id equals mainCont.MasterId into mainContGroup
+                           from mainContItem in mainContGroup.DefaultIfEmpty(new ParagraphPrimaryContent { MasterId = master.Id, Content = string.Empty })
+
+                           where master.Id == masterId
+                           group new { master, pic, metadata, mainContItem } by new { master.Id } into masterGroup
+                           let mainContentData = masterGroup.Select(f => f.mainContItem).Distinct().FirstOrDefault(f => f != null && f.Content.HasValue())
+                           let masterData = masterGroup.FirstOrDefault()
+                           let primaryPicData = masterGroup.Select(f => f.pic).Where(f => f.Path.HasValue()).FirstOrDefault(f => f.IsPrimaryBool)
+                           let picData = masterGroup.Select(f => f.pic).Distinct().Where(f => f.Path.HasValue()).OrderBy(f => f.Sequence)
+                           let metaData = masterGroup.Select(f => f.metadata).Distinct().OrderBy(f => f.Sequence)
+                                    .Where(item => item.TypeByEnum == MetadataType.Detail && item.Value.HasValue())
+
+                           select new PersonaViewModel
+                           {
+                               Name = masterData.master.Name,
+                               WikiPath = masterData.master.Route,
+                               PicturePrimaryPath = primaryPicData?.Path ?? "",
+                               PicturePrimaryCaption = primaryPicData?.Caption ?? "",
+                               Pictures = picData
+                                   .Select(f => new PictureViewModel
+                                   {
+                                       PicturePath = f.Path,
+                                       PictureCaption = f.Caption.HasValue() && f.Caption.Length >= ConfigData.MinLengthOfPictureCaption ? f.Caption : string.Empty,
+                                       Sequence = f.Sequence
+                                   }).ToList(),
+                               Metadatas = metaData
+                                    .Select(item => new MetadataViewModel
+                                    {
+                                        Key = item.Key,
+                                        Description = item.Value,
+                                        Sequence = item.Sequence,
+                                        GroupHeader = item.Value //Need to get the group header
+                                    }).ToList(),
+                               MainContent = mainContentData?.Content ?? "",
+                               Paragraphs = new List<Paragraph2ContentViewModel> { new Paragraph2ContentViewModel
+                            {
+                                Content = mainContentData.Content,
+                                Header2 = masterData.master.Name,
+                                Sequence = 0
+                            } }
+                           }).FirstOrDefault();
+
+            var parah2 = para2Data.Where(m => m.MasterId == masterId).ToList();
+            var parah3 = para3Data.Where(m => m.MasterId == masterId).ToList();
+            var parahContents = paraData.Where(m => m.MasterId == masterId).ToList();
+
+            if (parahContents.Any())
+            {
+                int sequence = 1;
+                foreach (var para2Item in parah2.OrderBy(f => f.Sequence))
+                {
+                    if (parahContents.Any(f => f.ParagraphHeader2Id != para2Item.Id))
+                    {
+                        persona.Paragraphs.Add(new Paragraph2ContentViewModel
+                        {
+                            Content = parahContents.FirstOrDefault(f => f.ParagraphHeader2Id == para2Item.Id)!.Content,
+                            Header2 = para2Item.Header,
+                            Para3s = new List<Paragraph3ContentViewModel>(),
+                            Sequence = sequence++
+                        });
+                    }
+                    else
+                    {
+                        persona.Paragraphs.Add(new Paragraph2ContentViewModel
+                        {
+                            Content = string.Empty,
+                            Header2 = "Details",
+                            Para3s = new List<Paragraph3ContentViewModel>(),
+                            Sequence = sequence++
+                        });
+                    }
+
+                    if (parah3.Any(f => f.ParagraphHeader2Id == para2Item.Id)) //Any items matching the para2 header
+                    {
+                        foreach (var para3Item in parah3.Where(f => f.ParagraphHeader2Id == para2Item.Id).OrderBy(f => f.Sequence))
+                        {
+                            if (parahContents.Any(f => f.ParagraphHeader2Id == para2Item.Id && f.ParagraphHeader3Id == para3Item.Id))
+                            {
+                                persona.Paragraphs.Last().Para3s!.Add(new Paragraph3ContentViewModel
+                                {
+                                    Content = parahContents.FirstOrDefault(f => f.ParagraphHeader2Id == para2Item.Id && f.ParagraphHeader3Id == para3Item.Id)!.Content,
+                                    Header3 = para3Item.Header,
+                                    Sequence = sequence++,
+                                });
+                            }
+                        }
+                    }
+
+                }
+            }
+            return persona;
+        }
+
+        [TestCaseSource(nameof(DatabaseFiles))]
+        public void Should_EachItem_Have_PrimaryContent(string dbFilePath)
+        {
+            ProcessConstants.DatabasePath = dbFilePath;
+            WikiDatabase wikiDatabase = new WikiDatabase();
+            WikiAppController wikiAppController = new WikiAppController(wikiDatabase);
+
+            var isPrimaryMetadataContentEnabled = wikiDatabase.PhoneSettingsRepository.IsPrimaryMetadatDisplayEnabled;
+            var primaryMetadataContentFields = wikiDatabase.PhoneSettingsRepository.PrimaryMetadatDisplayContent;
+
+            if (isPrimaryMetadataContentEnabled)
+            {
+                Assert.NotNull(primaryMetadataContentFields);
+                Assert.IsTrue(primaryMetadataContentFields.Any());
+            }
+
+            foreach (var menu in wikiAppController.AppMenuItems())
+            {
+                var items = wikiAppController.GetListOfWikiItems(new List<string> { menu.Tags });
+                foreach (var item in items)
+                {
+                    Assert.NotNull(item);
+                    if (isPrimaryMetadataContentEnabled)
+                    {
+                        Assert.IsTrue(item.PrimaryMetadataContent.Any());
+                        Assert.IsTrue(item.ShowPrimaryContentMetadata);
+                        foreach (var meta in item.PrimaryMetadataContent)
+                        {
+                            Assert.NotNull(meta);
+                            Assert.IsTrue(meta.Key.HasValue());
+                            Assert.IsTrue(meta.Description.HasValue());
+                            Assert.IsTrue(primaryMetadataContentFields.Contains(meta.Key));
+                        }
+                    }
+                    else
+                    {
+                        Assert.IsTrue(item.MainContent.HasValue());
+                        Assert.IsTrue(item.ShowPrimaryContentMetadata == false);
+                    }
                 }
             }
         }
@@ -110,7 +267,7 @@ namespace WikiExtractor.Tests
             {
                 return Directory.EnumerateFiles(
                     IoHelper.CombinePath(PjUtility.Runtime.ExecutingRepositoryRootFolder,
-                    "WikiExtractor\\bin\\Debug\\net6.0\\Db"), "*.db");
+                    "App\\Databases"), "*.db");
             }
         }
     }
