99fb803dcc3de65d5d6d7f692d857a503a6dfc37
peterrexj <peterrexj@hotmail.com>
2023-03-21 23:44:13 +1100

    fixing the tablets

--- App/GeneralInformation/Views/PersonaDetailPage.xaml.cs
+++ App/GeneralInformation/Views/PersonaDetailPage.xaml.cs
@@ -222,11 +222,24 @@ namespace GeneralInformation.Views
             if (sender != null)
             {
                 var width = ((SfBorder)sender).Bounds.Width;
+                if (width > 600) width = 600; //For Tablet with higher width, the width is set back to 600
                 var automationId = ((SfBorder)sender).AutomationId?.SplitAndTrim(",")?.ToList();
                 if (width > 0 && automationId?.Count() == 2)
                 {
+                    //item 0 - height
+                    //item 1 - width
+                    
                     var actualHeight = (automationId[0].ToDouble() / automationId[1].ToDouble()) * width;
                     ((SfBorder)sender).HeightRequest = actualHeight;
+                    //For tablets, since the width is shortend, the picture will sit in the centre with gaps around the border.
+                    //hence removing the border and radius
+                    if (width >= 600)
+                    {
+                        ((SfBorder)sender).WidthRequest = width;
+                        ((SfBorder)sender).BorderColor = Color.Transparent;
+                        ((SfBorder)sender).BorderWidth = 0;
+                        ((SfBorder)sender).CornerRadius = 0;
+                    }
                 }
             }
         }
