486597ebdbf2595bfcc54297a908827530dee299
peterrexj <peterrexj@hotmail.com>
2023-03-26 14:09:17 +1100

    main activity update common

--- App/Countries/CountriesOfWorld.Android/MainActivity.cs
+++ App/Countries/CountriesOfWorld.Android/MainActivity.cs
@@ -11,6 +11,7 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using System.IO;
 using Android.Gms.Ads;
+using Microsoft.AppCenter.Crashes;
 
 namespace CountriesOfWorld.Droid
 {
@@ -22,82 +23,34 @@ namespace CountriesOfWorld.Droid
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(CryptoHelper.Decrypt("+QcTizXqAlBO5w3z7WWab6WlQqM96TUQ8OC8rPLrTVeTC1plyteIfJwEGTzq1TCWGELzS3AF87Z2LeXwOcc0Y6WV67I9nEFo61Uz71rzy0evCgpa221FX0GMc1/eQIc6"));
             base.OnCreate(savedInstanceState);
 
+            AndroidEnvironment.UnhandledExceptionRaiser += AndroidEnvironment_UnhandledExceptionRaiser;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            DisplayCrashReport();
             MobileAds.Initialize(ApplicationContext);
 
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
 
         #region Error 
+        private void AndroidEnvironment_UnhandledExceptionRaiser(object sender, RaiseThrowableEventArgs e)
+        {
+            e.Handled = true;
+            Crashes.TrackError(e.Exception);
+        }
+
         private static void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
         {
             var newExc = new Exception("TaskSchedulerOnUnobservedTaskException", unobservedTaskExceptionEventArgs.Exception);
-            LogUnhandledException(newExc);
+            Crashes.TrackError(newExc);
         }
 
-        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            var newExc = new Exception("CurrentDomainOnUnhandledException", unhandledExceptionEventArgs.ExceptionObject as Exception);
-            LogUnhandledException(newExc);
-        }
-
-        internal static void LogUnhandledException(Exception exception)
-        {
-            try
-            {
-                const string errorFileName = "Fatal.log";
-                var libraryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); // iOS: Environment.SpecialFolder.Resources
-                var errorFilePath = Path.Combine(libraryPath, errorFileName);
-                var errorMessage = String.Format("Time: {0}\r\nError: Unhandled Exception\r\n{1}",
-                DateTime.Now, exception.ToString());
-                File.WriteAllText(errorFilePath, errorMessage);
-
-                // Log to Android Device Logging.
-                Android.Util.Log.Error("Crash Report", errorMessage);
-            }
-            catch
-            {
-                // just suppress any error logging exceptions
-            }
-        }
-
-        /// <summary>
-        // If there is an unhandled exception, the exception information is diplayed 
-        // on screen the next time the app is started (only in debug configuration)
-        /// </summary>
-        [Conditional("DEBUG")]
-        private void DisplayCrashReport()
-        {
-            const string errorFilename = "Fatal.log";
-            var libraryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-            var errorFilePath = Path.Combine(libraryPath, errorFilename);
-
-            if (!File.Exists(errorFilePath))
-            {
-                return;
-            }
-
-            var errorText = File.ReadAllText(errorFilePath);
-            new AlertDialog.Builder(this)
-                .SetPositiveButton("Clear", (sender, args) =>
-                {
-                    File.Delete(errorFilePath);
-                })
-                .SetNegativeButton("Close", (sender, args) =>
-                {
-                    // User pressed Close.
-                })
-                .SetMessage(errorText)
-                .SetTitle("Crash Report")
-                .Show();
+            var newExc = new Exception("CurrentDomainOnUnhandledException", e.ExceptionObject as Exception);
+            Crashes.TrackError(newExc);
         }
 
         #endregion
