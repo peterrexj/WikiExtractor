fe60afe97a4f7f73dd94720ec85357dc19f54aef
peterrexj <peterrexj@hotmail.com>
2023-03-26 14:27:43 +1100

    splash screen remove logs which is not required

--- App/Countries/CountriesOfWorld.Android/SplashActivity.cs
+++ App/Countries/CountriesOfWorld.Android/SplashActivity.cs
@@ -17,15 +17,11 @@ namespace CountriesOfWorld.Droid
     [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : AppCompatActivity
     {
-        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
-
         public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
         {
             base.OnCreate(savedInstanceState, persistentState);
-            Log.Debug(TAG, "SplashActivity.OnCreate");
         }
 
-        // Launches the startup task
         protected override void OnResume()
         {
             base.OnResume();
@@ -38,8 +34,6 @@ namespace CountriesOfWorld.Droid
 
         async void SimulateStartup()
         {
-            Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
-            Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
             //await Task.Delay(3000);
             StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
